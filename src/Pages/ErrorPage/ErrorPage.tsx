f21a977db0fbc97e234fbe7b7eedc1204cd1a2cc
kazymaliev <durusbekov779@example.com>
2024-05-23 14:28:44 +0600

    errorPage

--- src/Pages/ErrorPage/ErrorPage.tsx
+++ src/Pages/ErrorPage/ErrorPage.tsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import styles from "./ErrorPage.module.css"
+import React from "react";
+import styles from "./ErrorPage.module.css";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
-    <div>
-      error page
-    </div>
-  )
-}
+    <>
+      <div>
+        <p className={styles.pErr}>
+          Sorry, this page is not found in GeeksForGeeks !!!
+        </p>
+      </div>
+      <div>
+        <p className={styles.p2Err}>404</p>
+      </div>
+      <div>
+        <a href="/">
+          <button className={styles.btnErr}>go to home</button>
+        </a>
+      </div>
+    </>
+  );
+};
 
-export default ErrorPage
+export default ErrorPage;
